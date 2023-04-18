1a2a278740b01cf9e5a071293a38aa71718a6237
Fedir Katushonok <46378315+CeSiumUA@users.noreply.github.com>
2023-04-18 14:13:16 +0300

    Fixed record update

--- src/database.py
+++ src/database.py
@@ -20,7 +20,7 @@ class Database:
     
     def update_client(self, client):
         collection = self.__get_clients_collection()
-        collection.update_one({self.__client_index_name: client['client_id']}, {"$set": {"last_online": client['last_online']}, "$set": {"notified": client["notified"]}})
+        collection.update_one({self.__client_index_name: client['client_id']}, {"$set": {"last_online": client['last_online'], "notified": client['notified']}})
 
     def add_client(self, client):
         collection = self.__get_clients_collection()
